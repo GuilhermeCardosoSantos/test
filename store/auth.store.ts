9068856c69c7eae27d7cbaf14a0ed9c2ac9f8408
GuilhermeCardosoSantos <guifoxlokaum@gmail.com>
2026-04-27 14:42:52 -0300

    feat: add persistencia após refresh no store auth

--- store/auth.store.ts
+++ store/auth.store.ts
@@ -9,23 +9,27 @@ type User = {
 
 type AuthState = {
   user: User | null
-
   setAuth: (user: User) => void
   logout: () => void
 }
 
+const getUserFromStorage = (): User | null => {
+  if (typeof window === "undefined") return null
+
+  const stored = localStorage.getItem("user")
+  return stored ? JSON.parse(stored) : null
+}
+
 export const useAuthStore = create<AuthState>((set) => ({
-  user: null,
+  user: getUserFromStorage(), 
 
   setAuth: (user) => {
     localStorage.setItem("user", JSON.stringify(user))
-
     set({ user })
   },
 
   logout: () => {
     localStorage.removeItem("user")
-
     set({ user: null })
   },
 }))
